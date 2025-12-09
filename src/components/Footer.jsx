3fec464d5d78ddcea789e704f345d21bcf3b565e
madewild <max.de.wilde@ulb.be>
2025-12-09 11:16:13 +0100

    remove eact files

--- src/components/Footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="p-6 text-center border-t border-gray-200 dark:border-gray-700">
-      © 2025 Semantic Solutions – Tous droits réservés
-    </footer>
-  );
-}
